df280fee872fe9e541ca88cb4d24a9def2ede122
EmilienGts <emilien.gantois.pro@gmail.com>
2020-11-14 21:36:25 +0100

    implementation of typing

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './interfaces/todo.interface';
 
 @Injectable()
 export class TodosService {
-    todos = [
+    todos: Todo[] = [
         {
             id: 1,
             title: 'Todo 1',
+            done: false,
             description: 'I am the first todo',
         },
         {
             id: 2,
             title: 'Todo 2',
+            done: true,
             description: 'I am the second todo',
+        },
+        {
+            id: 3,
+            title: 'Todo 3',
+            done: true,
+            description: 'I am the third todo',
         },
     ];
     
-    findAll(): any[] {
+    findAll(): Todo[] {
         return this.todos;
     }
 }
